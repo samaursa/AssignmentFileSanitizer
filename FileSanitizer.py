23605932bbe4d9cef8f7cc6c747e8691078f27de
samaursa <saadrustam@gmail.com>
2016-12-02 17:06:01 -0500

    [MOD] Not finding the regex skips the file

--- FileSanitizer.py
+++ FileSanitizer.py
@@ -1,24 +1,37 @@
 import os
 import re
+import sys
 
-for root, dirs, files in os.walk("../"):
+dirToWalk = "."
+if len(sys.argv) == 2:
+    dirToWalk = sys.argv[1]
+
+if os.path.isdir(dirToWalk) is False:
+    print('Directory "{0}" does not exist'.format(dirToWalk))
+    exit()
+
+p = re.compile('100[0-9]{6}')
+for root, dirs, files in os.walk(dirToWalk):
     for file in files:
-        p = re.compile('100[0-9]{6}')
-        m = next(p.finditer(file))
-        newFileName = file[m.start():m.end()]
-        _, ext = os.path.splitext(file)
+        for itr in p.finditer(file):
+            m = next(p.finditer(file))
+            newFileName = file[m.start():m.end()]
+            _, ext = os.path.splitext(file)
 
-        oldFilePath = root + file
-        newFilePath = root + newFileName + ext
+            oldFilePath = root + "/" + file
+            newFilePath = root + "/" + newFileName + ext
 
-        if oldFilePath == newFilePath:
-            continue
+            print(oldFilePath)
 
-        if os.path.isfile(newFilePath):
-            print("File {} already exists".format(newFilePath))
-            continue
+            if oldFilePath == newFilePath:
+                continue
 
-        os.rename(oldFilePath, newFilePath)
+            if os.path.isfile(newFilePath):
+                print("File {} already exists".format(newFilePath))
+                continue
 
-        print("Renamed {} to {}".format(oldFilePath, newFilePath))
+            os.rename(oldFilePath, newFilePath)
+
+            print("Renamed {} to {}".format(oldFilePath, newFilePath))
+            break
     break
